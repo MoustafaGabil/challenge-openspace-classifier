b61325814b52ba8706c0ea8727ec1ba4ce831716
M.Gabil <127035455+mgabil@users.noreply.github.com>
2024-09-30 12:27:41 +0200

    the modified openspace file

--- utils/openspace.py
+++ utils/openspace.py
@@ -1,29 +1,37 @@
-
 from table import Table
-import random
+
 
 class Openspace:
     def __init__(self, tables_num=6):
-        self.tables = []  
-        for _ in range(tables_num):
-            self.tables.append(Table())  
         self.tables_num = tables_num
+        self.tables = []
+        for x in range(tables_num):
+            self.tables.append(Table())
 
-def organize(self, names):
-    
-    for i in names:
-        for j in self.tables:
-           j.append(Table.assign_seat())
+    def organize(self, names):
+        self.names = names
+        self.first_table = []
 
-def display(self):
-    for i in self.tables:
-                print(f"Table {i+1}:")
-                j=0 
-                for j in self.tables[k]:
-                    print(f"  Seat {j+1}: {seat()}")
-            
-                k = k+1
+        for name in self.names:
+            for table in self.tables:
+                for seat in Table.assign_seat():
+                    self.first_table.append(Table.assign_seat())
+        return self.first_table
 
 
+Lists = ["Majed", "Minah", "Amr", "JAck", "Leon"]
+open_1 = Openspace()
+print(open_1.organize(Lists))
+#     for i in names:
+#         for j in self.tables:
+#            j.append(Table.assign_seat())
 
-                
+
+# def display(self):
+#     for i in self.tables:
+#                 print(f"Table {i+1}:")
+#                 j=0
+#                 for j in self.tables[k]:
+#                     print(f"  Seat {j+1}: {seat()}")
+
+#                 k = k+1
--- utils/table.py
+++ utils/table.py
@@ -1,58 +1,60 @@
-List_names = ["Majed","Minah" ,"Amr", "JAck", "Leon"]
-import random
 
 class seat:
-    ''' This is the seat class to check if is free or not, where we can creat instance object'''
-    def __init__(self):
+    ''' This is the seat class to check if is free or not, if it is free so, it assigns a name to the free seat, It can also 
+        remove a person from a set and assign it as a free place '''
+    
+    def __init__(self):                # this constructor intiate the instance object (seat) with a free spot.
         self.free = True
         self.occupant = ""
-        
 
-    def set_occupant(self,name):
+    def set_occupant(self,name):      # this (function/method) assign a name to a certain seat.
         self.free = False
         self.occupant = name
     
-    def remove_occupant(self):
+    def remove_occupant(self):        # this (function/method) remove the an occupant (person) from the seat  
         self.free = True
         self.occupant = ""
-         
-    def __str__(self):
+        
+    def __str__(self):                # This function return the name of the occupant if it is assigned or "free seat" if it is free.
         if self.free:
             return f"free seat"
         else:
-            return f"{self.occupant}"
-        
+            return f"{self.occupant}" 
+
 
 ##################################              Class Table                 ##################################
 class Table:
-    """ This is the class to check the tables"""
-    def __init__(self, capacity = 4):   
+""" This class is mainly to assing 4 different persons to one table which is the maximum capacity for each table. 
+        It returns also the capacity of each table"""
+        
+    def __init__(self, capacity=4):
         self.capacity = capacity
-        self.seats = []              # this has to be the seats objects created from the seat class
-        for j in range (self.capacity):
-            self.seats.append(seat())    # here we are creating new seat and add to the list (object) 
-           
-    def showing_seats (self):             # looping over the 
+        self.seats = []  # this has to be the seats objects created from the seat class
+        for j in range(self.capacity):
+            self.seats.append(
+                seat()
+            )  # here we are creating new seat and add to the list (object)
+
+    def showing_seats(self):  # looping over the
         for i in self.seats:
-            print (i)
-          
+            print(i)
+
     def has_free_spot(self):
         for seat in self.seats:
             if seat.free:
                 return True
             else:
                 return False
-    
-    def assign_seat(self,name):
+
+    def assign_seat(self, name):
         for seat in self.seats:
             if seat.free:
                 seat.set_occupant(name)
-                return True 
-            
+                return True
 
     def left_capacity(self):
         left_free = 0
         for i in self.seats:
             if i.free:
-                left_free +=1
-        return  left_free
+                left_free += 1
+        return left_free
